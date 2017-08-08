12952acfc4acf1ce0a28c04212b3ca6303369c31
xhyoulegou <794621729@qq.com>
2017-08-09 02:11:16 +0800

    完成路由配置

---
+++ src/components/goods/Goods.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="goods">
+    测试11
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header">
+    <h1>测试</h1>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
