0625305f8bbb3106899b8ea44be1dbb8182e97a5
xhyoulegou <794621729@qq.com>
2017-08-09 19:02:21 +0800

    goods组件开发

--- src/components/goods/Goods.vue
+++ src/components/goods/Goods.vue
@@ -1,15 +1,91 @@
 <template>
   <div class="goods">
-    测试11
+    <div class="muen-wrapper">
+      <ul>
+        <li v-for="item in goods" class="muen-item">
+          <span class="text">
+            <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{ item.name }}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
 <script>
   export default {
-
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data () {
+      return {
+        goods: []
+      }
+    },
+    created () {
+      this.classMap = ['decrease', 'discount', 'guarantee', 'invoice', 'special']
+      this.$axios.get('http://118.184.85.83:8082/goods')
+      .then((res) => {
+        if (res.status === 200) {
+          this.goods = res.data[0].goods
+          console.log(this.goods)
+        }
+      })
+    }
   }
 </script>
 
-<style>
+<style lang="less">
+.goods{
+  display: flex;
+  position: absolute;
+  top: 174px;
+  bottom: 46px;
+  width: 100%;
+  overflow: hidden;
+  .muen-wrapper{
+    flex: 0 0 80px;
+    width: 80px;
+    background: #f3f5f7;
+    .muen-item{
+      display: table;
+      height: 54px;
+      width: 56px;
+      line-height: 14px;
+      font-size: 14px;
+      .icon {
+          display: inline-block;
+          vertical-align: top;
+          width: 12px;
+          height: 12px;
+          margin-right: 4px;
+          background-size: 12px 12px;
+          background-repeat: no-repeat;
+          &.decrease {
+          }
+          &.discount {
+          }
+          &.guarantee {
+          }
+          &.invoice {
+          }
+          &.special {
+          }
+        }
+    }
+  }
+  .foods-wrapper{
+    flex: 1;
 
+  }
+}
+.bg-image (@url) {
+  background-image: url("@{url}@2x.png");
+  @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3) {
+    background-image: url("@{url}@3x.png");
+  }
+}
 </style>
--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -1,11 +1,124 @@
 <template>
-  
+  <div class="star" :class="starType">
+    <span v-for="itemClass in itemClasses" :class="itemClass" class="star-item"></span>
+  </div>
 </template>
 
-<script>
-  
+<script type="text/ecmascript-6">
+const LENGTH = 5
+const CLS_ON = 'on'
+const CLS_HALF = 'half'
+const CLS_OFF = 'off'
+
+export default {
+  props: {
+    size: {
+      type: Number
+    },
+    score: {
+      type: Number
+    }
+  },
+  computed: {
+    starType () {
+      return 'star-' + this.size
+    },
+    itemClasses () {
+      let result = []
+      let score = Math.floor(this.score * 2) / 2
+      let hasDecimal = score % 1 !== 0
+      let integer = Math.floor(score)
+      for (let i = 0; i < integer; i++) {
+        result.push(CLS_ON)
+      }
+      if (hasDecimal) {
+        result.push(CLS_HALF)
+      }
+      while (result.length < LENGTH) {
+        result.push(CLS_OFF)
+      }
+      return result
+    }
+  }
+}
 </script>
 
-<style>
+<style scope lang="less">
+.star {
+  font-size: 0;
+  .star.item {
+    display: inline-block;
+    background-repeat: no-repeat;
+  }
+  &.star-48 {
+      .star-item {
+        width: 20px;
+        height: 20px;
+        margin-right: 22px;
+        background-size: 20px 20px;
+        display: inline-block;
+        &:last-child {
+          margin-right: 0;
+        }
+        &.on {
+          .bg-image(star48_on);
+        }
+        &.half {
+          .bg-image(star48_half);
+        }
+        &.off {
+          .bg-image(star48_off);
+        }
+      }
+    }
+    &.star-36 {
+      .star-item {
+        display: inline-block;
+        width: 15px;
+        height: 15px;
+        margin-right: 6px;
+        background-size: 15px 15px;
+        &:last-child {
+          margin-right: 0;
+        }
+        &.on {
+          .bg-image(star36_on);
+        }
+        &.half {
+          .bg-image(star36_half);
+        }
+        &.off {
+          .bg-image(star36_off);
+        }
+      }
+    }
+    &.star-24 {
+      .star-item {
+        display: inline-block;
+        width: 10px;
+        height: 10px;
+        margin-right: 3px;
+        background-size: 10px 10px;
+        &:last-child {
+          margin-right: 0;
+        }
+        &.on {
+          .bg-image(star24_on);
+        }
+        &.half {
+          .bg-image(star24_half);
+        }
+        &.off {
+          .bg-image(star24_off);
+        }
+      }
+    }
+}
 
+.bg-image (@url) {
+  background-image: url("@{url}@2x.png");
+  @media (-webkit-min-device-pixel-ratio: 3), (min-device-pixel-ratio: 3) {
+    background-image: url("@{url}@3x.png");
+  }
+}
 </style>
